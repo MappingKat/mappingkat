202a8a438dc974f53f4544fb701f352011a6a0b5
MappingKat <katrina@engelsted.co>
2015-03-22 21:51:26 -0600

    getting styling links to work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,48 +1,7 @@
 var NPMap,
   App = {
-    // current: null,
-    currentId: '',
+    current: null,
     data: {},
-    setId: function (newId) {
-      var narrative = document.getElementById('narrative'),
-      sections = narrative.getElementsByTagName('section'),
-      places = App.places;
-
-      if (newId === App.currentId) return;
-      for (var i = 0; i < places.length; i++){
-        if (App.places.feature.properties.id === newId) {
-          NPMap.config.L.fitBounds(new L.LatLngBounds(layer.getLatLng()));
-          // map.setView(layer.getLatLng(), layer.feature.properties.zoom || 14);
-        //  TODO:  change color of heat map when scroll to... 
-        //  layer.setIcon(L.mapbox.marker.icon({
-        //     'marker-color': '#a8f'
-        //   }));
-        // } else {
-        //   layer.setIcon(L.mapbox.marker.icon({
-        //     'marker-color': '#404040'
-        //   }));
-        }
-      }
-      
-      for (var i = 0; i < sections.length; i++) {
-        sections[i].className = sections[i].id === newId ? 'active' : '';
-      }
-      currentId = newId;
-    },
-    narrative: function () {
-      var newId = App.currentId,
-      narrative = document.getElementById('narrative'),
-      narrativeHeight = narrative.offsetHeight,
-      sections = narrative.getElementsByTagName('section');
-
-      for (var i = sections.length - 1; i >= 0; i--) {
-        var rect = sections[i].getBoundingClientRect();
-        if (rect.top >= 0 && rect.top <= narrativeHeight) {
-          newId = sections[i].id;
-        }
-      }
-      App.setId(newId);
-    },
     createLayer: function(id) {
       App.current = L.heatLayer(App.data[id], {
         radius: 50
@@ -81,7 +40,7 @@ var NPMap,
   };
 
 NPMap = {
-  baseLayers: false,
+  baseLayers: [{id: 'mapbox.pencil'}],
   center: {lat: 42.714732, lng: -110.56},
   div: 'map',
   smallzoomControl: {
@@ -108,59 +67,6 @@ NPMap = {
         }
       });
 
-      NPMap.config.L.addControl(new L.npmap.control.legend({
-        position: 'topleft',
-        html: '<article id="narrative"><div class="section prose">'+
-              '<section id="cover" class="cover active"><img src= "assets/img/kat.png" class="katrina"/>'+
-              '<h1 class="katrina">About</h1><ul><div id="social">'+
-                  '<li class="tada animated"><a href="mailto:katrina@engelsted.co"><img src="assets/img/gmail.png" alt="email"></a></li>'+
-                  '<li class="tada animated"><a href="www.linkedin.com/pub/katrina-engelsted/22/bb1/3b2/" target="_blank"><img src="assets/img/linkedin.png" alt="linkedin"></a></li>'+
-                  '<li class="tada animated"><a href="http://twitter.com/mappingkat/" target="_blank"><img src="assets/img/twitter.png" alt="twitter"></a></li>'+
-                  '<li class="tada animated"><a href="http://github.com/MappingKat/" target="_blank"><img src="assets/img/github.png" alt="github"></a></li>'+
-              '</div></ul>'+
-           '<p class="cover-para">With a background in <strong>geography</strong> and <strong>Asian studies</strong>, Katrina enjoys creating maps, collecting data, and spreading geographical knowledge. Currently, she works with the <strong>National Park Service</strong> to design and create interactive web maps. <br> From navigating with topographic maps in the mountains of Colorado to analyzing oil palm plantation imagery around Southeast Asia to teaching the Indonesian provincial government how to collect spatial data, Katrina has worked on a range of geospatial projects. Katrina aims to make the world more geograpically and map literate. She lives for making the world a <strong>mappier</strong> place.</p>'+
-          '</section>'
-        //   '<section id="nps">'+
-        //     '<h3>Web Developer @NPMap: National Park Service (March 2014 to present)</h3>'+
-        //     '<p>Katrina is working with the small NPMap team to help develop and design engaging maps for the National Park Service.  Her job also entails working with the OpenStreetMap community to get more people excited about mapping the parks.  The team focuses on three main projects that will enable the public and National Park employees to get more accurate and current geospatial data.  The NPBuilder, Places and <a href="http://www.nps.gov/npmap/park-tiles/">Park Tiles</a> all aim to create easier open source maps. Check out some of the work at <a href="http://www.nps.gov/npmap/">www.nps.gov</a>.</p>'+
-        //   '</section>'+
-        //  '<section id="gschool">'+
-        //     '<h3>Student @gschool (Sept 2013 to Feb 2014)</h3>'+
-        //     '<p>This intensive six-month web development program covered the ins-and-outs of Ruby on Rails and Javascript coding. With Jeff Casimir, Katrina Owen, Franklin Webber, and Jorge as instructors, 25 students worked on various projects to hone in their team work and programming skills.  Projects ranged in focus from'+
-        //       '<br> ‣ Practice using Sinatra and Rails'+
-        //       '<br> ‣ Test Driven Development(Rspec, Minitest, Capybara)'+
-        //       '<br> ‣ Multi-tendancy'+
-        //       '<br> ‣ API integration'+
-        //       '<br>We worked on group projects, as well as indiviual ones, which can all be found at <a href="http://github.com/MappingKat" target="_blank">www.github.com/MappingKat</a></p>'+
-        //   '</section>'+
-        //   '<section id="earthline">'+
-        //     '<h3>Production Manager @Earthline (Jan 2013 to Sept 2013)</h3>'+
-        //     '<p>‣ Wrote 30 page project reports so that customers could understand satellite imagery aquisition, mapping processes, and classification schemes for their estates'+
-        //     '<br>‣ Managed imagery, mapping, surveying and printshop departments (over 30 staff) and around ten projects at a time'+
-        //     '<br>‣ Coordinated with customers when problems arose or clarification was needed'+
-        //     '<br>‣ Worked with VTiger to organize leads, customers and sales'+
-        //     '<br>‣ Hired mapping managers, supervisors and editors; remote sensing operators; surveyors Constructed excel project database with basic macros to overview and track projects more effectively'+
-        //     '<br>PT Earthline is an Indonesian mapping company that works with various clients, such as WWF, ExxonMobil, Chevron, Wilmar, Genting Holdings, etc, to map oil palm plantations, mining concessions and green fields in Indonesia, Malaysia, and parts of Africa.</p>'+
-        //   '</section>'+
-        //   '<section id="hot">'+
-        //     '<h3>Technical Writer @Humanitarian OpenStreetMap Team (July to December 2013)</h3>'+
-        //     '<p>Various parts of Indonesia'+
-        //     '<br>‣ Edited and created chapters for OpenStreetMap learning and teaching tutorials (www.learnosm.org)<br>‣ Used Google Drive, Github and Jekyll/Hyde to share and edit chapters<br>‣ Edited and wrote workshops and project updates on www.openstreetmap.or.id<br>‣ Attended workshops throughtout Indonesia to teach OSM tools, such as PostGIS, PostgreSQL, and QGIS plugins</p><p>Humanitarian OpenStreetMap Team is an NGO set up to collect, map and teach OSM tools. This Indonesia project was funded by AusAID, World Bank, IOM and the Indonesian government to map critical infrastructure and create better contingency plans using the InaSafe plugin.</p>'+
-        //   '</section>'+
-        //   '<section id="allhands">'+
-        //     '<h3>Base Manager @All Hands (June 2011 to Feb 2012)</h3>'+
-        //     '<p>Volunteered on two projects:  one in Leogane, Haiti and the other in Cagayan de Oro, Mindanoa, Philippines. ‣ Managed 30+ volunteers at base and at work site <br> ‣ Organized travel,transportation, accommodation, food and work schedule for over 40 volunteers<br> ‣ Represented All Hands at conferences on disaster response, prevention and management<br> ‣ Organized excel charts and edited Google Earth maps/ KMLfiles layers for the water sanitation, rubble and demolition sites <br> ‣ Built schools, demolished unsafe structures, and cleared house foundations that had been damaged by the 2010 earthquake. All Hands is a disaster response organization that sends volunteers from all over the world to aid victims after natural disasters.</p>'+
-        //   '</section>'+
-        //   '<section id="bonobo">'+
-        //     '<h3>GIS Coordinator @Bonobo Conservation Initiative (Nov 2011 - Feb 2012)</h3>'+
-        //     '<p>‣ Gathered and compiled research data from biodiversity and forest surveys<br> ‣ Created interactive online map using Javascript, CartoCSS, and HTML (http://www.bonobo.org/peace-forest-map/)<br> ‣ Organized past folders and datasets so that workflow was quicker and more efficient<br> BCI is a NGO that aims to protect the bonobos, preserve their tropical rainforest habitat and empower local communities in the Congo Basin. </p>'+
-        //   '</section>'+
-        //   '<section id="colgate">'+
-        //     '<h3>Outdoor Educator @Colgate University (Sept 2007 - May 2011)</h3>'+
-        //     '<p>Taught peers, teachers and community members in the community various outdoor skills, such as survival, kayaking, camping, skiing, rock climbing, etc.  Learned how to conduct proper lessons and teach in different styles in order to appeal to a variety of learning types.</p>'+
-        //   '</section>'+
-        // '</div></article>'
-      }));
         //(c) 2014, Vladimir Agafonkin simpleheat, a tiny JavaScript library for drawing heatmaps with Canvas
          //https://github.com/mourner/simpleheat
         !function(){"use strict";function t(i){return this instanceof t?(this._canvas=i="string"==typeof i?document.getElementById(i):i,this._ctx=i.getContext("2d"),this._width=i.width,this._height=i.height,this._max=1,void this.clear()):new t(i)}t.prototype={defaultRadius:5,defaultGradient:{.01:"blue",.1:"cyan",.2:"lime",.3:"yellow",0.6:"red"},data:function(t){return this._data=t,this},max:function(t){return this._max=t,this},add:function(t){return this._data.push(t),this},clear:function(){return this._data=[],this},radius:function(t,i){i=i||15;var a=this._circle=document.createElement("canvas"),e=a.getContext("2d"),s=this._r=t+i;return a.width=a.height=2*s,e.shadowOffsetX=e.shadowOffsetY=200,e.shadowBlur=i,e.shadowColor="black",e.beginPath(),e.arc(s-200,s-200,t,0,2*Math.PI,!0),e.closePath(),e.fill(),this},gradient:function(t){var i=document.createElement("canvas"),a=i.getContext("2d"),e=a.createLinearGradient(0,0,0,256);i.width=1,i.height=256;for(var s in t)e.addColorStop(s,t[s]);return a.fillStyle=e,a.fillRect(0,0,1,256),this._grad=a.getImageData(0,0,1,256).data,this},draw:function(t){this._circle||this.radius(this.defaultRadius),this._grad||this.gradient(this.defaultGradient);var i=this._ctx;i.clearRect(0,0,this._width,this._height);for(var a,e=0,s=this._data.length;s>e;e++)a=this._data[e],i.globalAlpha=Math.max(a[2]/this._max,t||.05),i.drawImage(this._circle,a[0]-this._r,a[1]-this._r);var n=i.getImageData(0,0,this._width,this._height);return this._colorize(n.data,this._grad),i.putImageData(n,0,0),this},_colorize:function(t,i){for(var a,e=3,s=t.length;s>e;e+=4)a=4*t[e],a&&(t[e-3]=i[a],t[e-2]=i[a+1],t[e-1]=i[a+2])}},window.simpleheat=t}(),
@@ -174,11 +80,11 @@ NPMap = {
         App.to('work');
         callback();
       },
-      preinit: function(callback) {
-        var overlay = L.npmap.preset.baselayers.mapbox.pencil;
-        NPMap.config.overlays = [ overlay ];
-        callback();
-      }
+      // preinit: function(callback) {
+      //   var overlay = L.npmap.preset.baselayers.mapbox.pencil;
+      //   NPMap.config.overlays = [ overlay ];
+      //   callback();
+      // }
     },
     scrollWheelZoom: false,
     minZoom: 2,
@@ -211,7 +117,8 @@ var places = { type: 'FeatureCollection', features: [
     'properties': { 'id': 'colgate' }, type: 'Feature' }
 ]};
 
-  var placesLayer = L.npmap.layer.geojson({
+  var placesLayer = new L.FeatureGroup();
+  L.npmap.layer.geojson({
     data: places
   }),
   narrative = document.getElementById('narrative'),
